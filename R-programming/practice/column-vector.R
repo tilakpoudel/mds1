6c24847fff0812918aadbf057a0ffa6dc95be47e
tilak <tilak.poudel.314@gmail.com>
2025-02-22 21:52:26 +0545

    feat: r practice and assignments

---
+++ R-programming/practice/column-vector.R
@@ -0,0 +1,5 @@
+#Column vector
+x <- c(1:30)
+y <- x^3
+plot(x,y)
+
---
+++ R-programming/practice/script.R
@@ -0,0 +1,11 @@
+print("Hari bool! Welcome to R programming")
+x<-4
+y=5
+z = x+y
+
+message("Sum of x and y is ", x + y)
+message(sprintf("Sum of x and y is %d", x + y))
+print(sprintf("Sum of %d and %d is %d", x, y, x + y))
+
+print(paste("Sum of x and y is", z))
+
