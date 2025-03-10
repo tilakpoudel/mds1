0acf8a13bdb7a44bf44d1bcc667654a52693d4fc
tilak <tilak.poudel.314@gmail.com>
2025-03-10 22:14:53 +0545

    knit: unit1 session 1-5

--- R-programming/practice/user-def-function.R
+++ R-programming/practice/user-def-function.R
@@ -37,3 +37,19 @@ roll3 <- function(dice) {
 roll3(dice = 1:6)
 roll3(dice = 1:12)
 roll3(dice = 1:24)
+
+
+best_practice <- c("Let", "the", "computer", "do", "the", "work")
+print_words <- function(sentence) {
+  
+  print(sentence[1])
+  print(sentence[2])
+  print(sentence[3])
+  print(sentence[4])
+  print(sentence[5])
+  print(sentence[6])
+  
+}
+print_words(best_practice) # [1] ”Let” [1] “the” [1] “computer” [1] “do” [1] “the” [1] “work”
+print_words(best_practice[-6]) # [1] ”Let” [1] “the” [1] “computer” [1] “do” [1] “the” [1] “NA”
+best_practice[-6]
