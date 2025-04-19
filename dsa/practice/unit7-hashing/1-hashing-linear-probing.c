a5dd34e4b564f127c659fc68f9039391f0308666
tilak <tilak.poudel.314@gmail.com>
2025-04-19 07:28:26 +0545

    feat: add hashing with linear proboing

---
+++ dsa/practice/unit7-hashing/1-hashing-linear-probing.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+int a[10] = {NULL};
+
+int hash(int k)
+{
+    return k % 10;
+}
+
+void insert(int k)
+{
+    int i = hash(k), index;
+    // Linear probing
+    // If the index is already occupied, we need to find the next available index
+    // by checking the next indices in a circular manner
+    for(int j = 0; j < 10; j++)
+    {
+        index = (i + j) % 10;
+
+        if(a[index]==NULL)
+        {
+            a[index] = k;
+
+            break;
+        }
+    }
+}
+
+// search an element
+int search(int k)
+{
+    int i = hash(k), index;
+
+    for(int j = 0; j < 10; j++)
+    {
+        index = (i + j) % 10;
+
+        if(a[index]==k)
+        {
+            return index;
+        }
+
+    }
+
+    return -1;
+}
+
+void del(int k)
+{
+    int i = hash(k), index;
+
+    for(int j = 0; j < 10; j++)
+    {
+        index = (i + j) % 10;
+
+        if(a[index]==k)
+        {
+            a[index]=NULL;
+            break;
+        }
+    }
+}
+
+int main()
+{
+    insert(10);
+    insert(15);
+    insert(89);
+    insert(18);
+    insert(49);
+    insert(58);
+    insert(69);
+    insert(5);
+    del(58);
+
+    printf("%d\n", search(5));
+    printf("%d\n", search(58));
+}
